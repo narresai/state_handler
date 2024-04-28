3c9683e16dde4826e5790d8b08bb8cb53059b669
Narreddy Saini <narreddysaini1506@gmail.com>
2024-04-28 20:24:57 +0530

    using state inside class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state ={
+            active : "basketball"
+        }
+    }
+    render(){
+        let labels = ["basketball","Pubg","Tiger","Phone","Laptop","Cricket"];
+        return(
+            <>
+            <center>
+            <div>
+            {
+                labels.map((label)=>
+                    <button className={this.state.active === label ? "active" : "" }
+                    onClick ={()=>{
+                        this.setState({
+                            active : label
+                        })
+                    }}>{label}</button>)
+                }
+   
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            </div>
+            <img src={`/assets/${this.state.active}.jpg`} alt="this.state.active" />
+            </center>
+
+            </>
+        )
+    }
+
 }
 
 export default App;
